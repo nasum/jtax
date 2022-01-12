a4132bb680d1e6c330bf87160d911ea3bbafc557
nasum <tomato.wonder.life@gmail.com>
2022-01-13 00:55:31 +0900

    add addKastYearResult

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -9,3 +9,32 @@ describe.each([[500000, 2020]])("toString", (income, year) => {
     `);
   });
 });
+
+describe("addLastYearResult", () => {
+  describe.each([[500000, 2021, 500000, 2020]])(
+    "accept",
+    (income, year, income2, year2) => {
+      test("output", () => {
+        const jtax = new JTax(income, year);
+        const jtax2 = new JTax(income2, year2);
+        jtax.addLastYearResult(jtax2);
+        expect(jtax.toString()).toBe(`
+      year   : ${year}
+      income : ${income}
+    `);
+      });
+    }
+  );
+  describe.each([[500000, 2021, 500000, 2019]])(
+    "unaccept",
+    (income, year, income2, year2) => {
+      test("output", () => {
+        const jtax = new JTax(income, year);
+        const jtax2 = new JTax(income2, year2);
+        expect(() => jtax.addLastYearResult(jtax2)).toThrowError(
+          new Error(`Unacceptable: this year is ${year}, last year is ${year2}`)
+        );
+      });
+    }
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 export default class JTax {
   private income: number;
   private year: number;
+  private lastYearResult?: JTax;
 
   constructor(income: number, year: number) {
     this.income = income;
     this.year = year;
+  }
+
+  addLastYearResult(lastYearResult: JTax): void {
+    if (this.year - lastYearResult.year == 1) {
+      this.lastYearResult = lastYearResult;
+    } else {
+      throw new Error(
+        `Unacceptable: this year is ${this.year}, last year is ${lastYearResult.year}`
+      );
+    }
   }
 
   toString(): string {
